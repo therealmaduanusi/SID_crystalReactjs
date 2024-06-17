73f1cd34258cebb326ac1c1744c70fe4de95ef60
Victor Maduanusi <maduanusivictor2020@gmail.com>
2024-06-17 10:01:02 +0100

    prettier extention

--- src/Components/PageNotFound.jsx
+++ src/Components/PageNotFound.jsx
@@ -1,10 +1,16 @@
-import {NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function PageNotFound() {
   return (
     <>
-    <h1 className="text-center my-5">Page Not Found</h1>
-    <p className="text-center mb-5">Click <NavLink className='text-decoration-underline text-danger' to='/'>here</NavLink> to view site </p>
+      <h1 className="text-center my-5">Page Not Found</h1>
+      <p className="text-center mb-5">
+        Click{" "}
+        <NavLink className="text-decoration-underline text-danger" to="/">
+          here
+        </NavLink>{" "}
+        to view site{" "}
+      </p>
     </>
-  )
+  );
 }
